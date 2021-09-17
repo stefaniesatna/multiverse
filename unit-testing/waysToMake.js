e393e5170081e9e2f0a9bea58c3a710a1e0d0519
Stefanie Satna <heystef@fb.com>
2021-09-17 16:19:17 +0100

    waysToMake WIP

---
+++ unit-testing/waysToMake.js
@@ -0,0 +1,16 @@
+export default function waysToMake(sum, arr, i = 0){
+
+    if (target < 0){
+        return 0
+    }
+    if (target === 0){
+        return 1
+    }
+    
+    let ways = 0
+    for (let i = j; i < arr.length; i++){
+        ways+= waysToMake(target - arr[i], arr, j)
+    }
+
+    return count 
+}
